d7f4d315cd8402bd2f0e01dd86ec657aef300c01
Gesserok <bairu.auto@gmail.com>
2024-10-01 15:47:02 +0300

    start

---
+++ src/main/kotlin/com/example/demo/controllers/KubernetesTestController.kt
@@ -0,0 +1,12 @@
+package com.example.demo.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class KubernetesTestController {
+
+
+    @GetMapping("/kubernetes")
+    fun getWork() = "IT WORKS"
+}
